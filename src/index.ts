420cd2afd232c542708cc167b94fc5d2b02735ca
Lev Kowalski <levkowalski@gmail.com>
2019-11-20 10:56:23 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -63,10 +63,10 @@ function create() {
   const layer = map.createStaticLayer(0, tiles, 0, 0)
   platforms = this.physics.add.staticGroup()
 
-  platforms
-    .create(400, 568, 'ground')
-    .setScale(2)
-    .refreshBody()
+  // platforms
+  //   .create(400, 568, 'ground')
+  //   .setScale(2)
+  //   .refreshBody()
 
   // platforms.create(600, 400, 'ground')
   // platforms.create(50, 250, 'ground')
@@ -75,34 +75,34 @@ function create() {
   ghost.displayWidth = 48
   ghost.displayHeight = 48
 
-  player = this.physics.add.sprite(100, 450, 'dude')
+  // player = this.physics.add.sprite(100, 450, 'dude')
 
   ghost.setCollideWorldBounds(true)
   // player.setBounce(0.2)
-  player.setCollideWorldBounds(true)
-  this.physics.add.collider(player, platforms)
-  this.physics.add.collider(ghost, platforms)
-  this.physics.add.collider(player, layer)
+  // player.setCollideWorldBounds(true)
+  // this.physics.add.collider(player, platforms)
+  // this.physics.add.collider(ghost, platforms)
+  // this.physics.add.collider(player, layer)
 
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  })
+  // this.anims.create({
+  //   key: 'left',
+  //   frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+  //   frameRate: 10,
+  //   repeat: -1,
+  // })
 
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
-    frameRate: 20,
-  })
+  // this.anims.create({
+  //   key: 'turn',
+  //   frames: [{ key: 'dude', frame: 4 }],
+  //   frameRate: 20,
+  // })
 
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  })
+  // this.anims.create({
+  //   key: 'right',
+  //   frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+  //   frameRate: 10,
+  //   repeat: -1,
+  // })
   cursors = this.input.keyboard.createCursorKeys()
 
   // stars = this.physics.add.group({

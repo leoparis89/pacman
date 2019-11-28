92fa53c0a190fe6a691039a5d1ee88c7b1b93085
Lev Kowalski <levkowalski@gmail.com>
2019-11-28 22:33:20 +0100

    wip

--- src/utils/shapeGenerator.ts
+++ src/utils/shapeGenerator.ts
@@ -6,6 +6,7 @@ export const createRoom = (width: number, height: number, coords: Point) => {
 
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
+      // Left
       if (i === 0) {
         // Top Left corner
         if (j === 0) {
@@ -24,8 +25,30 @@ export const createRoom = (width: number, height: number, coords: Point) => {
           result.set([i + x, j + y], TileMap.wall.corner.bottom.left[1])
           continue
         }
-        // Left wall
         result.set([i + x, j + y], TileMap.wall.vertical.sample2[1])
+        continue
+      }
+
+      // Left
+      if (i === width - 1) {
+        // Top Left corner
+        if (j === 0) {
+          result.set([i + x, j + y], TileMap.wall.corner.top.right[0])
+          continue
+        } else if (j === 1) {
+          result.set([i + x, j + y], TileMap.wall.corner.top.right[1])
+          continue
+        }
+        // Bottom Left corner
+        if (j === height - 2) {
+          result.set([i + x, j + y], TileMap.wall.corner.bottom.right[0])
+          continue
+        }
+        if (j === height - 1) {
+          result.set([i + x, j + y], TileMap.wall.corner.bottom.right[1])
+          continue
+        }
+        result.set([i + x, j + y], TileMap.wall.vertical.sample1[1])
         continue
       }
       // result.set([i + x, j + y], TileMap.floor.sample1)

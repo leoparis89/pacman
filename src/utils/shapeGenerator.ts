da41167efac4f024dffe2a437df6a3788e48647e
Lev Kowalski <levkowalski@gmail.com>
2019-11-30 18:08:46 +0100

    wip

--- src/utils/shapeGenerator.ts
+++ src/utils/shapeGenerator.ts
@@ -6,25 +6,36 @@ export const createRoom = (width: number, height: number, coords: Point) => {
 
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
-      const current: [number, number] = [i + x, j + y]
+      const curr: [number, number] = [i + x, j + y]
+      const [currX, currY] = curr
+
       // Left
       if (i === 0) {
         if (j === 0) {
-          result.set([i + x, j + y - 2], TileMap.wall.corner.top.left[0])
-          result.set([i + x, j + y], TileMap.wall.vertical.sample2[1])
-        } else if (j === 1) {
-          result.set([i + x, j + y - 2], TileMap.wall.corner.top.left[1])
-          result.set([i + x, j + y], TileMap.wall.vertical.sample2[1])
+          result.set([currX, currY - 2], TileMap.wall.corner.top.left[0])
+          result.set([currX, currY - 1], TileMap.wall.corner.top.left[1])
+          result.set(curr, TileMap.wall.vertical.sample2[1])
         } else if (j === height - 1) {
-          // result.set([i + x, j + y - 2], TileMap.wall.vertical.sample2[1])
-          result.set([i + x, j + y - 1], TileMap.wall.corner.bottom.left[0])
-          result.set([i + x, j + y], TileMap.wall.corner.bottom.left[1])
+          result.set([currX, currY - 1], TileMap.wall.corner.bottom.left[0])
+          result.set(curr, TileMap.wall.corner.bottom.left[1])
         } else {
-          result.set([i + x, j + y], TileMap.wall.vertical.sample2[1])
+          result.set(curr, TileMap.wall.vertical.sample2[1])
         }
-        continue
       }
 
+      // Right
+      if (i === width - 1) {
+        if (j === 0) {
+          result.set([currX, currY - 2], TileMap.wall.corner.top.right[0])
+          result.set([currX, currY - 1], TileMap.wall.corner.top.right[1])
+          result.set(curr, TileMap.wall.vertical.sample1[1])
+        } else if (j === height - 1) {
+          result.set([currX, currY - 1], TileMap.wall.corner.bottom.right[0])
+          result.set(curr, TileMap.wall.corner.bottom.right[1])
+        } else {
+          result.set(curr, TileMap.wall.vertical.sample1[1])
+        }
+      }
       // if (i === width - 1) {
       //   if (j === 0) {
       //     result.set(current, TileMap.wall.corner.top.right[0])

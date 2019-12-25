2a816b63559a7538b4999da04348bce34e408925
Lev Kowalski <levkowalski@gmail.com>
2019-12-25 22:11:50 +0100

    wip

--- src/utils/createLevel.ts
+++ src/utils/createLevel.ts
@@ -4,6 +4,4 @@ export const createLevel = () => {
   // const levelWithRoom1 = insertRoom(newLevel, 3, 3, [0, 0])
 }
 
-export const addRoom = (levelState: LevelState, room: IRoom) => {}
-
 export const makeNewLevel = () => new Map()

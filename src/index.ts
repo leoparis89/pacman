f959bf40bb223c160a8fd2597c7fd6a01fa2872b
Lev Kowalski <levkowalski@gmail.com>
2019-12-27 23:20:59 +0100

    clean up

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { flow } from 'lodash'
 import Phaser from 'phaser'
 import settings from './settings'
 import { handleCursor } from './utils/controls'
-import { makeNewLevel } from './utils/createLevel'
 import { paintGrid, pointMaptoGrid } from './utils/helpers'
 import { roomReducer } from './utils/shapeGenerator'
-// import { insertRoom } from './utils/shapeGenerator'
 
 const { screen, tile } = settings
 
@@ -16,7 +14,6 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      // gravity: { y: 300 },
       debug: false,
     },
   },
@@ -49,12 +46,6 @@ function preload() {
     frameHeight: 32,
   })
 
-  // scene.load.image('dungeonTiles', 'assets/0x72_DungeonTilesetII_v1.3.png')
-  // scene.load.tilemapTiledJSON(
-  //   'dungeonMap',
-  //   'assets/0x72_DungeonTilesetII_v1.3.json',
-  // )
-
   scene.load.image('myTiles', 'assets/myTiles.png')
   scene.load.tilemapTiledJSON('myTilesMap', 'assets/myTiles.json')
 }
@@ -68,11 +59,6 @@ function create() {
   const scene: Phaser.Scene = this
 
   camera = scene.cameras.main
-
-  const newLevel = makeNewLevel()
-  // const levelWithRoom1 = insertRoom(newLevel, 3, 3, [1, 2])
-  // const levelWithRoom2 = insertRoom(levelWithRoom1, 5, 7, [6, 6])
-  // const levelWithRoom3 = insertRoom(levelWithRoom2, 4, 4, [15, 10])
 
   setup(scene)
 
@@ -105,86 +91,13 @@ function create() {
     frameRate: 10,
     repeat: -1,
   })
-  // cursors = this.input.keyboard.createCursorKeys()
-
-  // const controlConfig = {
-  //   camera: this.cameras.main,
-  //   left: cursors.left,
-  //   right: cursors.right,
-  //   up: cursors.up,
-  //   down: cursors.down,
-  //   acceleration: 0.04,
-  //   drag: 0.0005,
-  //   maxSpeed: 0.7,
-  // }
-  // controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig)
 }
-
-// function update() {
-//   const scene: Phaser.Scene = this
-//   const camera = scene.cameras.main
-
-//   if (cursors.left.isDown) {
-//     // console.log('left')
-//     console.log(scene.cameras)
-//     camera.scrollX = 20 // ghost.setVelocityX(-160)
-//     return
-//   }
-//   if (cursors.right.isDown) {
-//     console.log('right')
-//     // ghost.setVelocityX(160)
-//     // return
-//   }
-//   if (cursors.up.isDown) {
-//     console.log('up')
-//     // ghost.setVelocityY(-160)
-//     // return
-//   }
-//   if (cursors.down.isDown) {
-//     console.log('down')
-//     // ghost.setVelocityY(160)
-//     // return
-//   }
-//   // ghost.setVelocity(0)
-//   // // ghost.setX(70)
-//   // ghost.setX(gridPos.x + 24)
-//   // ghost.setY(gridPos.y + 24)
-//   // refreshPos(ghost)
-// }
 
 function update(time, delta) {
   handleCursor(cursors, character, stop)
 }
 
-// function refreshPos(ghost) {
-//   ghost.setX(gridPos.x * cellSize + cellSize / 2)
-//   ghost.setY(gridPos.y * cellSize + cellSize / 2)
-// }
-
-// const normalized = normalizeLevel(level, null)
-// const graph = gridToGraph(normalized)
-
-// const path = getShortestPath(graph, '0:0', '19:19')
-
-// let i = 0
-
-// if (path) {
-//   setInterval(() => {
-//     if (!path[i]) {
-//       return
-//     }
-//     const [x, y] = path[i].split(':').map(Number)
-//     gridPos = {
-//       x,
-//       y,
-//     }
-//     i++
-//   }, 100)
-// }
-
 const setup = (scene: Phaser.Scene) => {
-  // const level = levelPointMapToGrid(levelCoords)
-
   const level = roomReducer([
     { height: 3, width: 3, coords: [4, 4] },
     { height: 4, width: 3, coords: [9, 9] },
@@ -210,8 +123,7 @@ const setup = (scene: Phaser.Scene) => {
   // boundsLayer.scaleX = 3
   // boundsLayer.scaleY = 3
   // boundsMap.setCollision(388)
-  // boundsLayer.forEachTile((tile: Phaser.Tilemaps.Tile) => {})
-  // boundsLayer.setSize(1, 1)
+  // boundsLayer.forEachTile((tile: Phaser.Tilemaps.Tile) => {}) // boundsLayer.setSize(1, 1)
   // const image = scene.add.image(350, 350, 'ghost')
   // image.setDisplaySize(42, 42)
 

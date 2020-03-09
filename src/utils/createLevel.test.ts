df894fe6c5d7f15c44de71692137caf809800e72
Lev Kowalski <levkowalski@gmail.com>
2020-03-09 09:37:30 +0100

    clean up

--- src/utils/createLevel.test.ts
+++
@@ -1,7 +0,0 @@
-const mockLevel = new Map()
-
-describe('createLevel', () => {
-  it('should create initial room at (0,0)', () => {})
-})
-
-test('makeNewLevel should return an empty Level', () => {})
--- src/utils/createLevel.ts
+++
@@ -1,7 +0,0 @@
-export const createLevel = () => {
-  const LENGTH = 3
-  const newLevel = makeNewLevel()
-  // const levelWithRoom1 = insertRoom(newLevel, 3, 3, [0, 0])
-}
-
-export const makeNewLevel = () => new Map()

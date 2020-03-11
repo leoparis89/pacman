26d9fb3199af50ded583f2fd364c3025d8361f2d
Lev Kowalski <levkowalski@gmail.com>
2020-03-11 01:40:18 +0100

    wip

--- src/world/transformations.ts
+++ src/world/transformations.ts
@@ -6,6 +6,7 @@ export const roomToPointMap = (room: IRoom) => {
   const { height, width, coords } = room
   const [x, y] = coords
   const result: PointMap = new Map()
+
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
       result.set(JSON.stringify([i + x, j + y]), true)

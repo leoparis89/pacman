0d530868b3728bb2b4404173ec178be58b4ff380
Lev Kowalski <levkowalski@gmail.com>
2020-03-15 17:14:09 +0100

    wip

--- src/world/makeLevel.ts
+++ src/world/makeLevel.ts
@@ -3,26 +3,31 @@ import { roomsToPointMap } from './transformations'
 
 export const cookUpLevel = (
   seed: IRoom = { height: 9, width: 9, coords: [0, 0] },
-  steps: number = 10,
+  steps: number = 100,
 ): IRoom[] => {
   const result: IRoom[] = []
 
   result.push(seed)
 
-  for (let i = 0; i < steps; i++) {
-    // const level: PointMap = addRoomToPointMap(result)
+  let backTrack = 0
 
-    const curr = result[result.length - 1]
+  for (let i = 0; i < steps; i++) {
+    const curr = result[result.length - 1 - backTrack]
 
     const nextRoomDetails = getNextRoomDetails()
 
     const roomToAdd = nextRoom(result, curr, nextRoomDetails.dims)
 
     if (roomToAdd) {
+      backTrack = 0
       roomToAdd.type = nextRoomDetails.type
       result.push(roomToAdd)
+    } else {
+      backTrack++
+      steps++
     }
   }
+  console.log(result.length)
   return result
 }
 

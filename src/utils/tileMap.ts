fa1e34576a3659b75a9733d6a9c82888a748322b
Lev Kowalski <levkowalski@gmail.com>
2020-03-16 00:57:28 +0100

    add wall tilemap

--- src/utils/tileMap.ts
+++ src/utils/tileMap.ts
@@ -4,6 +4,12 @@ export default {
       clean: [5, 21],
       broken: [6, 22],
       tile: [7, 23],
+      wall: {
+        horizontal: {
+          clean: [2],
+          cracked: [36, 37],
+        },
+      },
     },
   },
 }

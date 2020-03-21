6009c73ce985f24c5802ce18687e6db7c3ed5452
Lev Kowalski <levkowalski@gmail.com>
2020-03-21 18:55:11 +0100

    wip

--- src/world/levelWrapping.ts
+++ src/world/levelWrapping.ts
@@ -1,6 +1,7 @@
 import { flow } from 'lodash'
 import { isFloor } from '../utils/tileCheckers'
 import tileMap from '../utils/tileMap'
+import { cloneMap, makeDirUtils, makeIsFree } from './levelWrappingUtils'
 
 export const wrapLevel = (level: PointMap) => {
   return flow(wrapEgedCases, wrapTrivialWalls)(level)
@@ -76,26 +77,4 @@ export const wrapEgedCases = (level: PointMap) => {
     }
   })
   return levelWithBorder
-}
-
-const cloneMap = (map: Map<any, any>) => {
-  const cloned = new Map()
-  map.forEach((val, key) => cloned.set(key, val))
-  return cloned
-}
-
-export const makeIsFree = (level: PointMap) => (coord: Point) =>
-  level.get(JSON.stringify(coord)) === undefined
-
-const makeDirUtils = ([x, y]: Point): { [index: string]: Point } => {
-  return {
-    up: [x, y - 1],
-    down: [x, y + 1],
-    left: [x - 1, y],
-    right: [x + 1, y],
-    upLeft: [x - 1, y - 1],
-    upRight: [x + 1, y - 1],
-    downLeft: [x - 1, y + 1],
-    downRight: [x + 1, y + 1],
-  }
 }
---
+++ src/world/levelWrappingUtils.ts
@@ -0,0 +1,21 @@
+export const cloneMap = (map: Map<any, any>) => {
+  const cloned = new Map()
+  map.forEach((val, key) => cloned.set(key, val))
+  return cloned
+}
+export const makeIsFree = (level: PointMap) => (coord: Point) =>
+  level.get(JSON.stringify(coord)) === undefined
+export const makeDirUtils = ([x, y]: Point): {
+  [index: string]: Point
+} => {
+  return {
+    up: [x, y - 1],
+    down: [x, y + 1],
+    left: [x - 1, y],
+    right: [x + 1, y],
+    upLeft: [x - 1, y - 1],
+    upRight: [x + 1, y - 1],
+    downLeft: [x - 1, y + 1],
+    downRight: [x + 1, y + 1],
+  }
+}

5498d34929a74a00afb0d05c083d17a266240c2a
Lev Kowalski <levkowalski@gmail.com>
2020-03-21 20:12:03 +0100

    wip

--- src/world/wrapping/levelWrapping.ts
+++ src/world/wrapping/levelWrapping.ts
@@ -9,34 +9,51 @@ import {
 } from './levelWrappingUtils'
 
 export const wrapLevel = (level: PointMap) => {
-  // return flow(wrapEgedCases, wrapTrivialWalls)(level)
-  return flow(wrapEgedCases)(level)
+  return flow(wrapEgedCases, wrapTrivialWalls)(level)
+  // return flow(wrapEgedCases)(level)
 }
 export const addEdgeCaseCorners = (level: PointMap) => {}
 
-// export const wrapTrivialWalls = (level: PointMap) => {
-//   // Clone fresh new level: levelWithBorder !
-//   const levelWithBorder = cloneMap(level)
-//   const isFree = makeIsEmpy(levelWithBorder)
+export const wrapTrivialWalls = (level: PointMap) => {
+  // Clone fresh new level: levelWithBorder !
+  const levelWithBorder = cloneMap(level)
+  const isFree = makeIsEmpy(levelWithBorder)
 
-//   level.forEach((tileValue, key) => {
-//     if (!isFloor(tileValue)) {
-//       return
-//     }
-//     const [x, y] = JSON.parse(key)
+  level.forEach((tileValue, key) => {
+    if (!isFloor(tileValue)) {
+      return
+    }
+    const [x, y] = JSON.parse(key)
 
-//     const dirs = makeDirUtils([x, y])
+    const dirs = makeDirUtils([x, y])
 
-//     if (isFree(dirs.up)) {
-//       // levelWithBorder.set(coordLeft, tileMap.blue.wall.corner.bottom.left)
-//       levelWithBorder.set(
-//         JSON.stringify(dirs.up),
-//         tileMap.blue.wall.horizontal.clean,
-//       )
-//     }
-//   })
-//   return levelWithBorder
-// }
+    if (isFree(dirs.up)) {
+      levelWithBorder.set(
+        JSON.stringify(dirs.up),
+        tileMap.blue.wall.horizontal.clean,
+      )
+    }
+    if (isFree(dirs.down)) {
+      levelWithBorder.set(
+        JSON.stringify(dirs.down),
+        tileMap.blue.wall.horizontal.clean,
+      )
+    }
+    if (isFree(dirs.left)) {
+      levelWithBorder.set(
+        JSON.stringify(dirs.left),
+        tileMap.blue.wall.vertical.clean,
+      )
+    }
+    if (isFree(dirs.right)) {
+      levelWithBorder.set(
+        JSON.stringify(dirs.right),
+        tileMap.blue.wall.vertical.clean,
+      )
+    }
+  })
+  return levelWithBorder
+}
 
 export const wrapEgedCases = (level: PointMap) => {
   // Clone fresh new level: levelWithBorder !

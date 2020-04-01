fbd40d06a1fbffa46d736222b124a07b0bdd435d
Lev Kowalski <levkowalski@gmail.com>
2020-04-01 10:04:20 +0200

    only warn !!

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'only-warn'],
   rules: {},
 }
